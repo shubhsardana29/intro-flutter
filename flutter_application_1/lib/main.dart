d827d14576d381f2ad0618475e2ef326cbdb03b3
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-12 08:55:27 +0530

    added colors and font

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
 
-void main() =>runApp(MaterialApp(
-  home:Scaffold(
-    appBar: AppBar(
-      title:Text("my first app"),
-      centerTitle: true,
-    ),
-    body: Center(
-      child: Text("Hello, World"),
-    ),
-    floatingActionButton: FloatingActionButton(
-      onPressed:(){},
-      child: Text("click"),
-    ),
-  ),
-));
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("my first app"),
+          centerTitle: true,
+          backgroundColor: Colors.black26,
+        ),
+        body: Center(
+            child: Text("Hello World!!",
+                style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    fontFamily: "IndieFlower",
+                    color: Colors.grey[600]))),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text("click"),
+          backgroundColor: Colors.black26,
+        ),
+      ),
+    ));
