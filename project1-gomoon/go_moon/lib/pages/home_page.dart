cf9e71bf40270deee4918df232303a6af34bba93
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-14 04:25:03 +0530

    added pageTitle

--- project1-gomoon/go_moon/lib/pages/home_page.dart
+++ project1-gomoon/go_moon/lib/pages/home_page.dart
@@ -6,11 +6,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/astro_moon.png"),
-          ),
+      body: _pageTitle(),
+      
+    );
+  }
+
+  Widget _pageTitle() {
+    return const Text(
+      "#GoMoon",
+      style: TextStyle(
+        color: Colors.white,
+        fontSize:70,
+        fontWeight: FontWeight.w800,
+      ),
+      );
+  }
+
+  Widget _astroImageWidget() {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage("assets/images/astro_moon.png"),
         ),
       ),
     );
