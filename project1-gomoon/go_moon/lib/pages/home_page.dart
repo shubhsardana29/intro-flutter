18e45c9b6e1075f95e057fa24e38bda78f88c5f7
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-14 05:35:06 +0530

    accessing device height and width

--- project1-gomoon/go_moon/lib/pages/home_page.dart
+++ project1-gomoon/go_moon/lib/pages/home_page.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  late double _deviceHeight, _deviceWidth;
+  HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: _pageTitle()
+          height: _deviceHeight,
+          width: _deviceWidth,
+          child: _pageTitle(),
           )
         ),
     );
