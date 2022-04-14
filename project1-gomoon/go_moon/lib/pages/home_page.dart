0b8372b52bb5ed5f8cc1d438a6da8c7f6c16861a
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-14 15:53:32 +0530

    added book-ride button

--- project1-gomoon/go_moon/lib/pages/home_page.dart
+++ project1-gomoon/go_moon/lib/pages/home_page.dart
@@ -101,7 +101,28 @@ class HomePage extends StatelessWidget {
         children: [
           _destinationDropDownWidget(),
           _travellersInformationWidget(),
+          _rideButton(),
         ],
+      ),
+    );
+  }
+
+  Widget _rideButton() {
+    return Container(
+      margin: EdgeInsets.only(bottom: _deviceHeight * 0.01),
+      width: _deviceWidth,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        onPressed: () {},
+        child: const Text(
+          "Book Ride!",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
       ),
     );
   }
