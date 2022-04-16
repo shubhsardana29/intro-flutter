6bdc6d25f0f55d90c7e64e82fb0cd9d2e9aa3501
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-16 06:47:54 +0530

    created stateful-widget

---
+++ project2-taskly/taskly/lib/pages/home_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage();
+
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  _HomePageState();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
