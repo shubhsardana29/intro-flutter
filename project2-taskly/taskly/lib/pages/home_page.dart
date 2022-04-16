41c78ada3074ff54be7a30144ca35a7badcbea21
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-16 07:09:54 +0530

    added appbar

--- project2-taskly/taskly/lib/pages/home_page.dart
+++ project2-taskly/taskly/lib/pages/home_page.dart
@@ -10,10 +10,21 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late double _deviceHeight, _deviceWidth;
   _HomePageState();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: _deviceHeight * 0.15,
+        title: const Text(
+          "Taskly!",
+          style: TextStyle(fontSize: 35),
+        ),
+      ),
+    );
   }
 }
