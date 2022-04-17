9e2961a34147ef6d1ce40a834b8bfd17be389c5e
Shubh Sardana <52607235+shubhsardana29@users.noreply.github.com>
2022-04-17 06:33:08 +0530

    initialized-hive(future)

--- project2-taskly/taskly/lib/main.dart
+++ project2-taskly/taskly/lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:taskly/pages/home_page.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter("hive_boxes");
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,4 +21,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
